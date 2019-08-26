e7006b6408693e8c35d1372b2710a7cbe66447bf
aisteron <timotheus@list.ru>
2019-08-26 17:54:33 +0300

    w26.08

--- themes/starter/src/js/index.js
+++ themes/starter/src/js/index.js
@@ -1,1 +1,14 @@
 import '../sass/style.sass'
+
+if(document.querySelector('ul.gallery'))
+{
+	let li = document.querySelectorAll('ul.gallery li');
+	li.forEach((e)=>{
+		e.addEventListener("click", replace)
+	})
+
+
+	function replace() {
+		document.querySelector('.main').setAttribute("src", this.querySelector('img').getAttribute('src'))
+	}
+}
